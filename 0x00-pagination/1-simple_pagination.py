6a8505641577c905e17f255db8d0ad2eaf92edcf
kaoutar <elazamikaoutar@gmail.com>
2024-04-03 19:58:11 +0000

    task1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Defining a class Server and a function"""
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns data in a specified page"""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+
+
